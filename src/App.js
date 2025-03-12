f505e9433184888f64bf878f712be9ba51e8189e
dzabbah <sergerufin1984@gmail.com>
2025-03-12 16:01:30 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ChildComponent from './ChildComponent';
+import "./App.css";
+
+// const App = () => {
+//   const [count, setCount] = useState(0);
+
+//   return (
+//     <div>
+//       <h1>Counter: {count}</h1>
+//       <button onClick={() => setCount(count + 1)}>Increment</button>
+//       <ChildComponent count={count} />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Compteur: {count} </h1>
+      <p id="counter">Nombre de Click: {count}</p>
+      <button id="btn" onClick={() => setCount(count + 1)}>Incrémenter</button>
+      <button id="negative" onClick={() => setCount(count - 1)}>Décrémenter</button>
+      <button id="reset" onClick={() => setCount(0)}>Réinitialiser</button>
+      <ChildComponent count={count} />
     </div>
   );
 }
---
+++ src/ChildComponent.js
@@ -0,0 +1,13 @@
+const ChildComponent = ({ count }) => {
+    return (
+      <div>
+        <p>Received count as prop: {count}</p>
+      </div>
+    );
+  }
+  
+
+
+
+  export default ChildComponent;
+  
